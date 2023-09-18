e92a25b602bda7d4e19349a9b8ef501117c5ef3c
Anthony Iacano <iacano5@outlook.com>
2023-09-18 17:31:40 -0400

    fixed bug where database would not generate on opening of notes page

--- routes/notes.js
+++ routes/notes.js
@@ -13,6 +13,12 @@ router.use(express.json());
 //basic GET call to send notes.html to display on /notes
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
+    //Creates valid JSON file synchronously if a file does not exist already
+    if(!(fs.existsSync(databasePATH))){
+      console.log("Database does not exist! Creating database JSON now...")
+      const output = fs.openSync(databasePATH,'w');
+      fs.writeSync(output, '[]')
+    }
 });
 //basic GET call to send styles.css to allow notes.html to display correctly
 router.get("/css/styles.css", (req, res) => {
