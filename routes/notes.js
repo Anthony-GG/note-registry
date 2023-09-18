4f5290afbc259449232a08a4597000f5cb0f9310
Anthony Iacano <iacano5@outlook.com>
2023-09-18 15:27:39 -0400

    added db creation process, added ability to add to database via post request

--- routes/notes.js
+++ routes/notes.js
@@ -3,10 +3,12 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const uuid = require('../helpers/uuid');
-const notes = require("../db/db.json")
 const fs = require('fs');
+const databasePATH = path.join(__dirname, "../db/db.json");
 
 router.use(express.json());
+
+
 
 //basic GET call to send notes.html to display on /notes
 router.get("/", (req, res) => {
@@ -20,44 +22,59 @@ router.get('/data', (req, res) => {
 router.post('/data', (req, res) => {
     // Log that a POST request was received
     console.info(`${req.method} request received to add a review`);
-  
-    // Destructuring assignment for the items in req.body
-const { title, text } = req.body;
 
-    // If all the required properties are present
-    if (title && text) {
-      // Variable for the object we will save
-      const newNote = {
-        title,
-        text,
-        review_id: uuid(),
-      };
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-
-      console.log(response);
-
-      //Adds new object to json file
-      var notesData = notes;
-      notesData.push(newNote);
-      noteDataJSON = JSON.stringify(notesData);
-      console.log(notesData);
-      console.log(noteDataJSON);
-
-
-      res.status(201).json(response);
-    
-      req.on('end', function(){
-        fs.writeFile(notes , noteDataJSON, function(err) {
-            if(err) {console.log(err);
-            }});
-      })
-    } else {
-      res.status(500).json('Error in posting review');
+    //Creates valid JSON file synchronously if a file does not exist already
+    if(!(fs.existsSync(databasePATH))){
+      console.log("Database does not exist! Creating database JSON now...")
+      const output = fs.openSync(databasePATH,'w');
+      fs.writeSync(output, '[]')
     }
+    
+    //Reads the existing JSON file and extracts data from it to be rewritten adding info added from POST request
+    fs.readFile(databasePATH, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Parse the existing JSON data
+        const existingData = JSON.parse(data);
+        
+            // Destructuring assignment for the items in req.body
+            const { title, text } = req.body;
+
+            // If all the required properties are present
+            if (title && text) {
+              // Variable for the object we will save
+              const newNote = {
+                title,
+                text,
+                note_id: uuid(),
+              };
+
+              const response = {
+                status: 'success',
+                body: newNote,
+              };
+
+
+
+
+              //Adds new object to json file
+              existingData.push(newNote);
+              noteDataJSON = JSON.stringify(existingData);
+              
+
+              //Rewrites info to JSON file with new object
+              fs.writeFile(databasePATH , noteDataJSON, function(err) {
+                  if(err) {console.log(err);
+                  }});
+
+
+              res.status(201).json(response);
+            } else {
+              res.status(500).json('Error in posting review');
+            }
+      }
+    });
   });
 
 //exports file to be used in server.js
